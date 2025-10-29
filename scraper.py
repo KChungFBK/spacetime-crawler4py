d4f799577c2957e4457cc0ecbc83f9496f106d0f
KChungFBK <kchung12@uci.edu>
2025-10-29 15:15:37 -0700

    Agent ID and linkfiltering
    Add useragent ID and filter links for uci.edu

--- scraper.py
+++ scraper.py
@@ -3,7 +3,14 @@ from urllib.parse import urlparse
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    # return [link for link in links if is_valid(link)]
+
+    # print(links)
+    for each in links:
+        if is_valid(each):
+            print(each)
+
+    return []
 
 def extract_next_links(url, resp):
     # Implementation required.
@@ -15,7 +22,17 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    
+    if (resp.status == 200): # No problem
+        content = resp.raw_response.content.decode('utf-8', 'ignore')
+        
+        links = re.findall(r'<a[^>]+href=[\'"]([^\'"]+)[\'"]', content)
+        links = [urlparse(link).geturl() for link in links]
+        
+    else:
+        print("Error: ", resp.error)  
+        
+    return links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -25,7 +42,11 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
+        isUCIEDU = re.search(r".uci.edu", parsed.path.lower())
+        if isUCIEDU:
+            print(f"Found Path: {parsed.path.lower()}")
+            
+            return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -34,7 +55,10 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-
+    
+        # else:
+        #     return False
+        
     except TypeError:
         print ("TypeError for ", parsed)
         raise
